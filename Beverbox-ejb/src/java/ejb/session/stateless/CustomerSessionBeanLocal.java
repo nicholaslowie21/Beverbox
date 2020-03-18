b0ebb0198fcb9de9e8febe81c31425c89a57b22c
zhixuanlee <zhixuan.lee@u.nus.edu>
2020-03-18 20:46:54 +0800

    feat: added basic methods for customersessionbean

--- Beverbox-ejb/src/java/ejb/session/stateless/CustomerSessionBeanLocal.java
+++ Beverbox-ejb/src/java/ejb/session/stateless/CustomerSessionBeanLocal.java
@@ -1,8 +1,20 @@
 package ejb.session.stateless;
 
+import entity.Customer;
+import java.util.List;
 import javax.ejb.Local;
+import util.exception.CreateNewCustomerException;
+import util.exception.CustomerNotFoundException;
 
+/**
+ *
+ * @author zhixuan
+ */
 @Local
 public interface CustomerSessionBeanLocal {
-    
+ 
+    public Long createNewCustomer(Customer newCustomer) throws CreateNewCustomerException;    
+    public List<Customer> retrieveAllCustomers();
+    public Customer retrieveCustomerByCustomerId(Long customerId) throws CustomerNotFoundException;
+    public Customer retrieveCustomerByCustomerName(String customerName) throws CustomerNotFoundException;
 }
