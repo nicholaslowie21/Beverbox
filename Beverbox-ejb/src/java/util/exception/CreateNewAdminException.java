caa4aa38e9772657ce1220874dc3f9a439e25a9f
zhixuanlee <zhixuan.lee@u.nus.edu>
2020-03-18 21:39:12 +0800

    feat: added CRUD methods for admin session bean

---
+++ Beverbox-ejb/src/java/util/exception/CreateNewAdminException.java
@@ -0,0 +1,15 @@
+package util.exception;
+
+/**
+ *
+ * @author zhixuan
+ */
+public class CreateNewAdminException extends Exception {
+
+    public CreateNewAdminException() {
+    }
+
+    public CreateNewAdminException(String msg) {
+        super(msg);
+    }
+}
