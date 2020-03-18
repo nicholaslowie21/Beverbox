11f3cd582e4ff73a2a5f9870eed767a68ef3fec6
nicholaslowie21 <nicholaslowie@comp.nus.edu.sg>
2020-03-18 15:42:22 +0800

    created Lowie entity, add relationship in beverage

---
+++ Beverbox-ejb/src/java/entity/Transaction.java
@@ -0,0 +1,136 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * @author User
+ */
+@Entity
+public class Transaction implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long transactionId;
+
+    @Column(nullable = false, unique = true, length = 32)
+    @NotNull
+    @Size(max = 32)
+    private String ccNum;
+    
+    @Column(nullable = false)
+    @NotNull
+    private Double transactionAmt;
+    
+    @ManyToOne(optional = false)
+    private Customer customer;
+
+    @ManyToOne(optional = true)
+    private Subscription subscription;
+    
+    @ManyToOne(optional = true)
+    private Promotion promotion;
+    
+    @OneToMany(mappedBy = "transaction")
+    private List<Beverage> beverages;
+    
+    public Long getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(Long transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public String getCcNum() {
+        return ccNum;
+    }
+
+    public void setCcNum(String ccNum) {
+        this.ccNum = ccNum;
+    }
+
+    public Double getTransactionAmt() {
+        return transactionAmt;
+    }
+
+    public void setTransactionAmt(Double transactionAmt) {
+        this.transactionAmt = transactionAmt;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Subscription getSubscription() {
+        return subscription;
+    }
+
+    public void setSubscription(Subscription subscription) {
+        this.subscription = subscription;
+    }
+
+    public Promotion getPromotion() {
+        return promotion;
+    }
+
+    public void setPromotion(Promotion promotion) {
+        this.promotion = promotion;
+    }
+
+    public List<Beverage> getBeverages() {
+        return beverages;
+    }
+
+    public void setBeverages(List<Beverage> beverages) {
+        this.beverages = beverages;
+    }
+    
+    
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (transactionId != null ? transactionId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the transactionId fields are not set
+        if (!(object instanceof Transaction)) {
+            return false;
+        }
+        Transaction other = (Transaction) object;
+        if ((this.transactionId == null && other.transactionId != null) || (this.transactionId != null && !this.transactionId.equals(other.transactionId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Transaction[ id=" + transactionId + " ]";
+    }
+    
+}
