1efe9600da51d26dae2cfd430d2f05b95a6a3203
nicholaslowie21 <nicholaslowie@comp.nus.edu.sg>
2020-04-07 21:01:24 +0800

    added security check for Rsw Request

--- BeverboxRsw/src/java/ws/restful/model/RenewSubReq.java
+++ BeverboxRsw/src/java/ws/restful/model/RenewSubReq.java
@@ -10,6 +10,9 @@ package ws.restful.model;
  * @author User
  */
 public class RenewSubReq {
+    
+    private String email;
+    private String password;
     private String promoCode;
     private boolean cashback;
     private long subsId;
@@ -18,12 +21,34 @@ public class RenewSubReq {
     public RenewSubReq() {
     }
 
-    public RenewSubReq(String promoCode, boolean cashback, long subsId, long custId) {
+    public RenewSubReq(String email, String password, String promoCode, boolean cashback, long subsId, long custId) {
+        this.email = email;
+        this.password = password;
         this.promoCode = promoCode;
         this.cashback = cashback;
         this.subsId = subsId;
         this.custId = custId;
     }
+
+    
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
     
     public String getPromoCode() {
         return promoCode;
